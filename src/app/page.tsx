9522e4c958603e047056a2da0142ad92a42786a5
Hannes Knauss <hc.knauss@web.de>
2024-06-19 23:24:13 +0200

    removed tRPC greeting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   return (
@@ -16,10 +15,6 @@ export default async function Home() {
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
         <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-
           <div className="flex flex-col items-center justify-center gap-4">
             {session && (
               <p className="text-center text-2xl text-white">
